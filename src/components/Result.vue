678f0408e69165811f6d6ecbc4261b37f9942e15
yuu <heinhtun1994@gmail.com>
2023-05-09 14:25:44 +0630

    second commit

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,5 +1,14 @@
 <template>
     <p>Result is {{ score }} ms</p>
+    <div class="" v-if="score<300">
+        <p>You are So fast !! Congrats...</p>
+    </div>
+    <div class="" v-else-if="score>300 && score<1500">
+        <p>You are Normal ...</p>
+    </div>
+    <div class="" v-else>
+        <p>You are so slow .Try Again!!</p>
+    </div>
 </template>
 <script>
 export default {
